cc16931fe125460d67020f2c88d4ea302d2bcf50
rlanesum <mudzingwa1@tritons.iowacentral.edu>
2022-09-05 22:52:26 -0500

    Add project files.

---
+++ Lab2B/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+string r1;
+string r2;
+
+int main() 
+{
+	const string r1 = " *  *  *  *";
+	const string r2 = "*  *  *  *";
+
+	cout << r1 << endl;
+	cout << r2 << endl;
+	cout << r1 << endl;
+	cout << r2 << endl;
+	cout << r1 << endl;
+	cout << r2 << endl;
+	cout << r1 << endl;
+	cout << r2 << endl;
+
+
+	return 0;
+}
